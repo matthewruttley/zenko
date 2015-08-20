35374b1eacb2c46dcb3a697a9a8e58ade422ca2d
Matthew Ruttley <mruttley@mozilla.com>
2015-08-20 15:51:32 -0400

    Table filling now working

--- static/js/overview_functionality.js
+++ static/js/overview_functionality.js
@@ -1,5 +1,12 @@
 //Functionality for Yahoo Category dynamic overview page
 
+//SETTINGS
+
+var original_columns = ['impressions', 'clicks', 'pins', 'blocks']
+var columns_with_metrics = ['impressions', 'clicks', 'ctr', 'pins', 'blocks', 'eng', 'eng_grade']
+
+//FUNCTIONALITY
+
 function spinner_handler(command){
 	//Handles the spinner dialog
 	spinner_dialog = document.getElementById('spinner_dialog')
@@ -22,7 +29,7 @@ function spinner_handler(command){
 		cell.appendChild(text)
 		
 		//add to table
-		spinner_table.appendChild(cell)
+		spinner_table.appendChild(row)
 	}
 }
 
@@ -37,34 +44,39 @@ function get_data_from_server() {
 	{
 		if (xmlhttp.readyState==4 && xmlhttp.status==200)
 		{
-			//parse the response
 			spinner_handler('Parsing response')
-			data = JSON.parse(xmlhttp.responseText)
-			
-			//process data
-			spinner_handler('Formatting data')
-			data = process_data(data)
-			
-			//activate graphs
-			spinner_handler('Setting up graphs')
-			set_up_graphs(data)
-			
-			//fill table
-			spinner_handler('Filling out tables')
-			fill_table_based_on_date_range(data)
-			
-			//close spinner
-			spinner_handler('end')
+			response = JSON.parse(xmlhttp.responseText)
+			setup_page(response)
 		}
 	}
 	xmlhttp.open("GET","/get_yahoo_overview_data", true);
 	xmlhttp.send();
 }
 
+function setup_page(data) {
+	//parse the respons
+	spinner_handler('start')
+	
+	//process data
+	spinner_handler('Formatting data')
+	data = process_data(data)
+	
+	//activate graphs
+	spinner_handler('Setting up graphs')
+	//set_up_graphs(data)
+	
+	//fill table
+	spinner_handler('Filling out tables')
+	fill_table_based_on_date_range(data)
+	
+	//close spinner
+	spinner_handler('end')
+}
+
 function set_up_graphs(data){
 	//Activate highcharts graphs
 	
-	columns = ['impressions', 'clicks', 'ctr', 'pins', 'blocks', 'eng', 'eng_grade']
+	
 	graph_data = {}
 	
 	//convert data into useful format for graphs
@@ -110,19 +122,132 @@ function set_up_graphs(data){
 	});
 }
 
-function fill_table_based_on_date_range(){
-	//Fill out each table cell with the correct data, potentially according to a date range
+function set_up_category_rows(data, earliest_date=false, latest_date=false) {
+	//set + check parameters
+	if (earliest_date == false) {
+		earliest_date = Date.UTC(2014,8,1)
+	}
+	if (latest_date == false) {
+		latest_date = Date.now()
+	}
 	
+	//aggregate the data according to the date range
+	category_rows = {} 
+	for (category of Object.keys(data)) {
+		//sum up the totals for each column
+		dates = Object.keys(data[category])
+		
+		category_split = category.split('___')
+		category_name = category_split[0]
+		column_name = category_split[1]
+		column_index = original_columns.indexOf(column_name)
+		
+		if (category_rows.hasOwnProperty(category_name) == false) {
+			category_rows[category_name] = []
+			for (var x in original_columns) {
+				category_rows[category_name].push(0)
+			}
+		}
+		
+		for (date of dates){
+			date_int = parseInt(date)
+			//console.log('category', category, "ld", latest_date, 'ed', earliest_date, 'date', date, date_int)
+			if ((date_int <= latest_date) && (date_int >= earliest_date)) {
+				if (data[category].hasOwnProperty(date)) {
+					//console.log('addition', category_rows[category_name][column_index], data[category][date][column_index])
+					category_rows[category_name][column_index] += data[category][date]
+				}
+			}
+		}
+	}
+	
+	//now set up ctr and engagement+grade
+	
+	for (category of Object.keys(category_rows)) {
+		
+		//get the data
+		row = category_rows[category]
+		
+		//calculate metrics
+		clickthrough = ctr(row[0], row[1])
+		eng = engagement(row[1], row[3])
+		eng_grade = engagement_grade(eng)
+		
+		//insert
+		row.splice(2, 0, clickthrough)
+		row.push(eng, eng_grade)
+		
+		//replace
+		category_rows[category] = row
+	}
+	
+	console.log('ping2')
+	
+	return category_rows
 }
 
-function elementwise_sum(list1, list2) {
-	//Element wise sum of two lists
-	//Assumes the lists are of the same length
-	sum = []
-	for (i=0;i<list1.length;i++) {
-		sum.push(list1[i] + list2[i])
+function fill_table_based_on_date_range(data, earliest_date=false, latest_date=false){
+	//Fill out each table cell with the correct data, potentially according to a date range
+	
+	data_rows = set_up_category_rows(data, earliest_date=false, latest_date=false)
+	
+	category_table_container = document.getElementById('category_table_container')
+	if (category_table_container.children.length != 0) { //remove the table if it exists
+		category_table_container.removeChild(category_table_container.childNodes[0])
 	}
-	return sum
+	
+	table = document.createElement('table')
+	table.className = "table table-responsive table-striped table-hover sortable"
+	category_table_container.appendChild(table)
+	
+	//header row
+	thead = document.createElement('thead')
+	header_row = document.createElement('tr')
+	header_cols = columns_with_metrics
+	header_cols.unshift('Category') //prepends the string
+	for (column of header_cols) {
+		cell = document.createElement('th')
+		//have to capitalize the column name
+		column_name = column.charAt(0).toUpperCase() + column.slice(1)
+		text = document.createTextNode(column_name.replace('_', ' '))
+		cell.appendChild(text)
+		header_row.appendChild(cell)
+	}
+	thead.appendChild(header_row)
+	table.appendChild(thead)
+	
+	//body
+	body = document.createElement('tbody')
+	table.appendChild(body)
+	
+	//get the column sums for each category
+	//data is in the format: {category:{date:[0,0,0,0,0,0]}}
+	
+	sorted_category_rows = Object.keys(data_rows)
+	sorted_category_rows.sort()
+	
+	for (category of sorted_category_rows) {
+		
+		//set up category row
+		category_row = document.createElement('tr')
+		
+		//add the name
+		category_name_cell = document.createElement('td')
+		category_name_text = document.createTextNode(category)
+		category_name_cell.appendChild(category_name_text)
+		category_row.appendChild(category_name_cell)
+		
+		//add the data
+		for (entry of data_rows[category]) {
+			cell = document.createElement('td')
+			text = document.createTextNode(entry)
+			cell.appendChild(text)
+			category_row.appendChild(cell)
+		}
+		
+		body.appendChild(category_row)
+	}
+	
 }
 
 function extract_date(date_string) {
@@ -132,23 +257,13 @@ function extract_date(date_string) {
 	return date
 }
 
-function insert_metrics(data) {
-	//accepts an array like: [impressions, clicks, pins, blocks]
-	//returns an array like: [impressions, clicks, ctr, pins, blocks, engagement, engagement_grade]
-	
-	//insert ctr
-	
-	if (data[0]===0) {
-		ctr = 0
+function ctr(impressions, clicks) {
+	//calculate and return ctr
+	if (impressions===0) {
+		return 0
 	}else{
-		ctr = +((data[1] / data[0]) * 100).toFixed(5) //hack found on SO
+		return +((clicks / impressions) * 100).toFixed(5)
 	}
-	data.splice(2, 0, ctr)
-	
-	//append engagement
-	
-	
-	
 }
 
 function engagement_grade(engagement){
@@ -191,16 +306,25 @@ function engagement(blocks, clicks){
 }
 
 function process_data(data){
-	//Process data at the start
+	//Processes data at the start
+	//	Starting format:
+	//	data = [[2281, "2015-8-3", 7498, 50, 1, 116], [2279, "2015-8-3", 3320, 22, 3, 98], ...]
+	//
+	//	Ending format:
+	//	data = {
+	//		'cat1___field': {
+	//			'date1': [1,2,3,4,5,6,7],
+	//			'date2': [1,2,3,4,5,6,7],
+	//		}
+	//	}
 	
 	//create id to category lookup
 	id_to_category = {}
-	categories = Object.keys(data.yahoo_filter.categories)
-	for (x=0;x<categories.length;x++) {
-		category = categories[x]
-		tile_ids = data.yahoo_filter.categories[category]
-		for (t=0;t<tile_ids.length;t++) {
-			id_to_category[tile_ids[t]] = category
+	categories = []
+	for (entry of yahoo_filter.categories) {
+		categories.push(entry[0])
+		for (tile_id of entry[1].split(',')) {
+			id_to_category[tile_id] = entry[0]
 		}
 	}
 	
@@ -208,38 +332,56 @@ function process_data(data){
 	//also get date range at the same time
 	category_aggregate = {}
 	date_range = {}
-	for (x = 0; x < data.length; x++){
+	for (entry of data){
 		
-		date = extract_date(data[x][1])
+		//extract date
+		date = extract_date(entry[1])
 		date_range[date] = true
 		
-		category = id_to_category[data[x][0]]
+		//iterate through columns
+		totals = entry.slice(2)
+		for (i in totals) {
+			column_name = original_columns[i]
+			total = totals[i]
+			
+			//setup name
+			category = id_to_category[entry[0]]
+			category_header = category + "___" + column_name //concatenated for highcharts later on
+			
+			//make sure category obj is set up correctly
+			//and put the data in the object
+			if (category_aggregate.hasOwnProperty(category_header) === false) {
+				category_aggregate[category_header] = {}
+			}
+			if (category_aggregate[category_header].hasOwnProperty(date) === false) {
+				category_aggregate[category_header][date] = total
+			}else{
+				category_aggregate[category_header][date] += total
+			}
 		
-		//make sure category obj is set up correctly
-		//and put the data in the object
-		if (category_aggregate.hasOwnProperty(category) === false) {
-			category_aggregate[category] = {}
-		}
-		if (category_aggregate[category].hasOwnProperty(date) === false) {
-			category_aggregate[category][date] = data[x].slice(2)
-		}else{
-			category_aggregate[category][date] = elementwise_sum(category_aggregate[category], data[x].slice(2))
 		}
 	}
 	
 	//convert date range to an array and sort ascending
 	date_range = Object.keys(date_range).sort()
 	
-	//columns = ['impressions', 'clicks', 'ctr', 'pins', 'blocks', 'eng', 'eng_grade']
-	
-	//insert CTR, engagement and engagement grade
-	for (var category of Object.keys(category_aggregate)){
-		for (var date of Object.keys(category_aggregate[category])) {
-			data = category_aggregate[category][date]
-			data = insert_metrics(data)
-			category_aggregate[category][date] = data
-		}
-	}
-	
 	return category_aggregate
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
